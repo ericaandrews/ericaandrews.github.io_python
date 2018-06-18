4b979e0ceaae053795bb10675ea5260b58eda615
ericaandrews <eureka.web.dev@gmail.com>
2018-06-18 14:55:06 -0700

    Create Earth.py

---
+++ Earth.py
@@ -0,0 +1,12 @@
+#This program calculates the users "moon weight" every year for 16 years based on the user's
+# "initial weight" and "increased weight each year". 
+def moon_weight(weight,increase_weight):
+    for year in range(1,16):
+        weight= weight + increase_weight
+        moon_weight= weight * 0.165
+        print('Year %s = %s moon weight'%(year,moon_weight))
+
+moon_weight(30,0.25)
+
+
+
