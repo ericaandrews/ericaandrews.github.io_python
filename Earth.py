31c3b6426a14b5fc81bcd945bd25c5f8929cd313
ericaandrews <eureka.web.dev@gmail.com>
2018-06-18 14:55:23 -0700

    Update Earth.py

--- Earth.py
+++ Earth.py
@@ -1,5 +1,6 @@
 #This program calculates the users "moon weight" every year for 16 years based on the user's
 # "initial weight" and "increased weight each year". 
+
 def moon_weight(weight,increase_weight):
     for year in range(1,16):
         weight= weight + increase_weight
