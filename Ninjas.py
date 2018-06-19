e6f7cb28c75a6eb5343605b78d6904d39268bbc0
ericaandrews <eureka.web.dev@gmail.com>
2018-06-18 17:36:51 -0700

    Create Ninjas.py

---
+++ Ninjas.py
@@ -0,0 +1,34 @@
+#This program prints strings in if statements that calculates whether a value assigned to a
+#total will display a print statement. 
+
+ninjas = 5
+if ninjas <=50 and ninjas>=30:
+    print("That\'s too many")
+if ninjas <=30 and ninjas>=10:
+    print("It\'ll be a struggle, but I can take\'em")
+if ninjas <10:
+    print ("I can fight those ninjas!")
+    
+ninjas = 20
+if ninjas <=50 and ninjas>=30:
+    print("That\'s too many")
+if ninjas <=30 and ninjas>=10:
+    print("It\'ll be a struggle, but I can take\'em")
+if ninjas <10:
+    print ("I can fight those ninjas!")
+    
+ninjas = 40 
+if ninjas <=50 and ninjas>=30:
+    print("That\'s too many")
+if ninjas <=30 and ninjas>=10:
+    print("It\'ll be a struggle, but I can take\'em")
+if ninjas <10:
+    print ("I can fight those ninjas!")
+
+ninjas = 5
+if ninjas < 10:
+    print ("I can fight those ninjas!")
+elif ninjas < 30:
+    print ("It\'ll be a struggle, but I can tak\'em")
+elif ninjas < 50:
+    print ("That's too many")
