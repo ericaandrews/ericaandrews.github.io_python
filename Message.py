06609b2194522749bafe4086336c2b3333beeb37
ericaandrews <eureka.web.dev@gmail.com>
2018-06-25 11:09:59 -0700

    Update Message.py

--- Message.py
+++ Message.py
@@ -2,7 +2,7 @@
 #The user discovers the "hidden" message by splitting the words within the string.
 #Once the user has split the string, the program displays the puzzles hidden message.
 
-hidden_message= 'this if is you not are a reading very this good then way you to have hide done a it message wrong'
-split_words= hidden_message.split()
+hidden_message = 'this if is you not are a reading very this good then way you to have hide done a it message wrong'
+split_words = hidden_message.split()
 for x in range(0, len(split_words), 2):
     print(split_words[x])
