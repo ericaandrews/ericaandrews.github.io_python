6aa84a80e669170334be8775232ced96068dd236
ericaandrews <eureka.web.dev@gmail.com>
2018-06-24 19:47:03 -0700

    Update Puzzle.py

--- Puzzle.py
+++ Puzzle.py
@@ -1,15 +1,15 @@
 #Combine lists
-games= ['reading', 'walking', 'cooking', 'driving', 'hanging out']
+games = ['reading', 'walking', 'cooking', 'driving', 'hanging out']
 print(games)
-foods= ['pastas', 'sushi', 'tacos', 'sandwitches', 'salads']
+foods = ['pastas', 'sushi', 'tacos', 'sandwitches', 'salads']
 print(foods)
-favorites= (games + foods)
+favorites = (games + foods)
 print (favorites)
 
 #Variable substitution
-ninjas= 3 * 25
+ninjas = 3 * 25
 print (ninjas)
-samurai= 2 * 40
+samurai = 2 * 40
 print (samurai)
 total_in_battle= ninjas + samurai
 print (total_in_battle)
@@ -22,6 +22,6 @@ samurai_per_tunnel = 40
 print ((roofs * ninjas_per_roof) + (tunnels * samurai_per_tunnel))
 
 #Placeholders
-first_name= "Erica"
-last_name= "Andrews"
+first_name = "Erica"
+last_name = "Andrews"
 print('"Hi there, %s %s"' % (first_name, last_name))
