9406e737a1b4f02d99f6acd3aac1b0bdb7a9bfbc
ericaandrews <eureka.web.dev@gmail.com>
2018-06-28 04:36:36 -0700

    Update Puzzles.py

--- Puzzles.py
+++ Puzzles.py
@@ -483,3 +483,20 @@ import time
 	t4.forward(50)
 	t4.left(90)
 	t4.forward(50)
+
+	#Octagon
+	import turtle
+	t = turtle.Pen()
+	def octagon(size, filled):
+	    if filled == True:
+		t.begin_fill()
+	    for x in range(1,9):
+		t.forward(size)
+		t.right(45)
+	    if filled == True:
+		t.end_fill()
+
+	t.color(1, 0.85, 0)
+	octagon(40, True)
+	t.color(0, 0, 0)
+	octagon(40, False)
