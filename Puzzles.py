d4f965a68e53d77e4aded30a686fd5750b54c23e
ericaandrews <eureka.web.dev@gmail.com>
2018-06-29 07:46:31 -0700

    Update Puzzles.py

--- Puzzles.py
+++ Puzzles.py
@@ -575,3 +575,14 @@ while 1:
 	    random_triangle()
 	    tk.update()
 
+	#Circle
+	import turtle
+	t = turtle.Pen()
+
+	def mycircle(red, green, blue):
+	    t.color(red, green, blue)
+	    t.begin_fill()
+	    t.circle(50)
+	    t.end_fill()
+
+	mycircle(0, 0.5, 0)
