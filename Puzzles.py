916032cdcc2370b78e5c23446a30a0fb6bf0ce5f
ericaandrews <eureka.web.dev@gmail.com>
2018-06-29 19:26:51 -0700

    Update Puzzles.py

--- Puzzles.py
+++ Puzzles.py
@@ -609,3 +609,13 @@ while 1:
 	canvas.create_arc(10, 240, 200, 320, extent=180, style=ARC)
 	canvas.create_arc(10, 320, 200, 400, extent=359, style=ARC)
 	tk.update()
+	
+	#Polygons
+	from tkinter import *
+	import time
+	tk = Tk()
+	canvas = Canvas(tk, width=400,height=400)
+	canvas.pack()
+	canvas.create_polygon(10, 10, 100, 10, 100, 110, fill="", outline="black")
+	canvas.create_polygon(200, 10, 240, 30, 120, 100, 140, 120, fill="", outline="black")
+	tk.update()
