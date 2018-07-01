19649ac949acac1bae1063bbde058ba78629a7b0
ericaandrews <eureka.web.dev@gmail.com>
2018-06-30 19:23:10 -0700

    Update Puzzles.py

--- Puzzles.py
+++ Puzzles.py
@@ -640,3 +640,17 @@ while 1:
 	    t.forward(100)
 	    t.left(225)
 	time.sleep(0.05)
+	
+	#Square within a square
+	import turtle
+	t = turtle.Pen()
+	def mysquare(size, filled):
+	    if filled == True:
+		t.begin_fill()
+	    for x in range(0,4):
+		t.forward(size)
+		t.left(90)
+	    if filled == True:
+		t.end_fill()
+	mysquare(50, True)
+	mysquare(150, False)
