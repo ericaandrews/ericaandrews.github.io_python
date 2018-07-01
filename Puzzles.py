6dba9861da605792f11648cab326cd7381f01e7d
ericaandrews <eureka.web.dev@gmail.com>
2018-06-30 19:42:02 -0700

    Update Puzzles.py

--- Puzzles.py
+++ Puzzles.py
@@ -662,4 +662,12 @@ while 1:
 	for x in range(1,5):
 	    t.forward(50)
 	    t.left(90)
-	time.sleep(0.05)    
+	time.sleep(0.05) 
+	
+	#Horizontal Rectangle
+	from tkinter import *
+	tk = Tk()
+	canvas = Canvas(tk, width=400,height=400)
+	canvas.pack()
+	canvas.create_rectangle(100, 100, 300, 150)
+	tk.update()
