4cbcae65330ee30a380fb9d9a933365e8104caaf
ericaandrews <eureka.web.dev@gmail.com>
2018-07-02 15:11:18 -0700

    Update Puzzles.py

--- Puzzles.py
+++ Puzzles.py
@@ -793,3 +793,27 @@ while 1:
 	t.begin_fill() 
 	t.circle(50) 
 	t.end_fill()
+	
+	#Moving triangle
+	import time
+	from tkinter import *
+	tk = Tk()
+	canvas = Canvas(tk, width=400, height=400)
+	canvas.pack()
+	canvas.create_polygon(10, 10, 10, 60, 50, 35)
+	for x in range(0, 35):
+	    canvas.move(1, 10, 0)
+	    tk.update()
+	    time.sleep(0.05)
+	for x in range(0, 34):
+	    canvas.move(1, 0, 10)
+	    tk.update()
+	    time.sleep(0.05)
+	for x in range(0, 35):
+	    canvas.move(1, -10, 0)
+	    tk.update()
+	    time.sleep(0.05)
+	for x in range(0, 34):
+	    canvas.move(1, 0, -10)
+	    tk.update()
+	    time.sleep(0.05)
