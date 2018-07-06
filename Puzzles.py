e559beb1f49d1be369d12b52b037672e4d3d429d
ericaandrews <eureka.web.dev@gmail.com>
2018-07-05 18:25:58 -0700

    Update Puzzles.py

--- Puzzles.py
+++ Puzzles.py
@@ -259,6 +259,38 @@ favorite_sports= {'Ralph Williams' : 'Football',
 	silly_strings = '''He said, "Aren't can't shouldn't wouldn't."'''
 	print (silly_strings)
 
+	
+#Boolean Testing    
+	print(bool(0))
+	print(bool())
+	print(bool(None))
+	print(bool(5))
+	print(bool(3.14))
+	print(bool(-500))
+	print(bool(' '))
+	print(bool('a'))
+
+	print(bool(0))
+	print(bool())
+	print(bool(None))
+	print(bool(5))
+	print(bool(3.14))
+	print(bool(-500))
+	print(bool(' '))
+	print(bool('a'))
+
+	my_list=[]
+	print(bool(my_list))
+	my_list=['silly', 'list','my','my','my']
+	print(bool(my_list))
+	my_map={'cookie:milk', 'bread:butter', 'blue:water'}
+	print(bool(my_map))
+
+	year= input('Year of birth: ')
+	if not bool(year.rstrip()):
+	print("You need to enter a value for your year of birth")
+
+	
 #Animation
 import time
 from tkinter import *
