901cd06d990678c422b15e0bdc0f23297d2e3ad0
ericaandrews <eureka.web.dev@gmail.com>
2018-07-06 20:35:53 -0700

    Update Puzzles.py

--- Puzzles.py
+++ Puzzles.py
@@ -31,6 +31,21 @@
 	last_name = "LastName"
 	print('"Hi there, %s %s"' % (first_name, last_name))
 
+	myscore= 1000
+	message= 'I scored %s points'
+	print (message % myscore)
+	
+	joke_text= '%s : a device for finding furniture in the dark'
+	bodypart1= 'Knee'
+	bodypart2= 'Shin'
+	print (joke_text % bodypart1)
+	print (joke_text % bodypart2)	
+	
+	nums= 'what did the number %s say to the number %s? Nice belt!!'
+	print (nums % (0,8))
+	nums= 'what did the number %s say to the number %s? Nice belt!!'
+	print (nums % (8,0))
+	
 	space = ' '*25
 	print ('%s 12 Name Street'% space)
 	print ('%s Twinkle Heath'% space)
