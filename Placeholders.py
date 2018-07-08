eff1b47391f7cb324de3bd08e4087a5abe58926c
ericaandrews <eureka.web.dev@gmail.com>
2018-07-08 09:18:34 -0700

    Create Placeholders.py

---
+++ Placeholders.py
@@ -0,0 +1,33 @@
+#Placeholders
+	first_name = "FirstName"
+	last_name = "LastName"
+	print('"Hi there, %s %s"' % (first_name, last_name))
+
+	myscore= 1000
+	message= 'I scored %s points'
+	print (message % myscore)
+	
+	joke_text= '%s : a device for finding furniture in the dark'
+	bodypart1= 'Knee'
+	bodypart2= 'Shin'
+	print (joke_text % bodypart1)
+	print (joke_text % bodypart2)	
+	
+	nums= 'what did the number %s say to the number %s? Nice belt!!'
+	print (nums % (0,8))
+	nums= 'what did the number %s say to the number %s? Nice belt!!'
+	print (nums % (8,0))
+	
+	space = ' '*25
+	print ('%s 12 Name Street'% space)
+	print ('%s Twinkle Heath'% space)
+	print ('%s West Sno'% space)
+	print ()
+	print ('Dear Sir')
+	print ()
+	print ('I wish to report that tiles are missing from the')
+	print ('outside roof.')
+	print ('I think it was wind the other night that blew them away.')
+	print ()
+	print ('Regards')
+	print ('Malcolm Dithering')
