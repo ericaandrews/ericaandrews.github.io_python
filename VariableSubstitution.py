11ce9fea0e1e1984a2283f58bdd025f79a7d60ce
ericaandrews <eureka.web.dev@gmail.com>
2018-07-08 09:18:01 -0700

    Create VariableSubstitution.py

---
+++ VariableSubstitution.py
@@ -0,0 +1,14 @@
+#Variable substitution
+	ninjas = 3 * 25
+	print (ninjas)
+	samurai = 2 * 40
+	print (samurai)
+	total_in_battle= ninjas + samurai
+	print (total_in_battle)
+	print ((3*25) + (2*40))
+
+	roofs = 3
+	ninjas_per_roof = 25
+	tunnels = 2
+	samurai_per_tunnel = 40
+	print ((roofs * ninjas_per_roof) + (tunnels * samurai_per_tunnel))
