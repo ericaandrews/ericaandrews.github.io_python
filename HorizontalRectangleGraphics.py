9e43c4d281ca3e71c85e3acee9c5ce05331bb520
ericaandrews <eureka.web.dev@gmail.com>
2018-07-08 21:07:07 -0700

    Delete HorizontalRectangleGraphics.py

--- HorizontalRectangleGraphics.py
+++
@@ -1,7 +0,0 @@
-#Horizontal Rectangle Graphics
-	from tkinter import *
-	tk = Tk()
-	canvas = Canvas(tk, width=400,height=400)
-	canvas.pack()
-	canvas.create_rectangle(100, 100, 300, 150)
-	tk.update()
