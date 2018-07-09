2df33aaf1998017b0d36ed967b0ef2fc17c54d3b
ericaandrews <eureka.web.dev@gmail.com>
2018-07-08 18:26:14 -0700

    Create Random.py

---
+++ Random.py
@@ -0,0 +1,10 @@
+#Random
+import random
+	deserts= ['cookies', 'ice cream', 'sundaes', 'brownies','cake']
+	print(random.choice(deserts))
+	print(random.choice(deserts))
+
+	random.shuffle(deserts)
+	print(deserts)
+	random.shuffle(deserts)
+	print(deserts)
