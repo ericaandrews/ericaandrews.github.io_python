1a39e55388c9480dc124fbd758d4660533ad742b
ericaandrews <eureka.web.dev@gmail.com>
2018-07-08 18:25:51 -0700

    Create ClassMethod.py

---
+++ ClassMethod.py
@@ -0,0 +1,10 @@
+#Class Method	
+	class Giraffes:
+	    def __init__(self, spots):
+		self.giraffe_spots = spots
+
+	ozwald = Giraffes(100)
+	gertrude = Giraffes(150)
+	print(ozwald.giraffe_spots)
+	print(gertrude.giraffe_spots)
+	
