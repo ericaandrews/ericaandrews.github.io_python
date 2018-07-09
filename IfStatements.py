535b4a73cabb4a021cb5c0307ce15f148d544b4c
ericaandrews <eureka.web.dev@gmail.com>
2018-07-08 17:01:43 -0700

    Create IfStatements.py

---
+++ IfStatements.py
@@ -0,0 +1,63 @@
+#If statements
+	age = 25
+	if age > 20:
+	    print('Why are you here?')
+
+	age = 10
+	if age > 10:
+	    print('You are too old for my jokes!')  
+	age = 10
+	if age >= 10:
+	    print('You are too old for my jokes!')
+	age = 10
+	if age == 10:
+	    print('What\'s brown and sticky? A stick!!')
+
+	age = 12
+	if age == 12:
+	    print ("A pig fell in the mud!")
+	else:
+	    print("Shh, It's a secret.")
+
+	age = 12
+	if age == 10:
+	    print ("What do you call an unhappy cranberry?")
+	    print ("A blueberry!")
+	elif age == 11:
+	    print ("What did the green grape say to the blue grape?")
+	    print ("Breathe! Breathe!")
+	elif age == 12:
+	    print ("What did 0 say to 8?")
+	    print ("Hi guys!")
+	elif age == 13:
+	    print ("Why wasn't 10 afraid of 7?")
+	    print ("Because rather than eating 9, 7 8, pi.")
+	else:
+	    print ("Huh?")
+
+	if age >=10 and age <= 13:
+	    print ('What is 13 + 49 + 84 + 155 + 97? A headache!')
+	else:
+	    print ('Huh?')
+
+	amount = 800
+	if (amount >= 100 and amount <= 500) or (amount >= 1000 and amount <= 5000):
+	    print('amount is between 100 & 500 or between 1000 & 5000')
+	amount = 400
+	if (amount >= 100 and amount <= 500) or (amount >= 1000 and amount <= 5000):
+	    print('amount is between 100 & 500 or between 1000 & 5000')
+	amount = 3000
+	if (amount >= 100 and amount <= 500) or (amount >= 1000 and amount <= 5000):
+	    print('amount is between 100 & 500 or between 1000 & 5000')
+
+	ninjas = 5
+	if ninjas < 10:
+	    print("I can fight those ninjas!")
+	elif ninjas < 30:
+	    print("It'll be a struggle, but I can take 'em")
+	elif ninjas < 50:
+	    print("That's too many")
+
+	twinkies = 600
+	if twinkies < 100 or twinkies > 500:
+	    print('Too few or too many')    
