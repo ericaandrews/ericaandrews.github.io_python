ae1a1adafd6e0e0d9391a8cddfaac068a4ea5131
ericaandrews <eureka.web.dev@gmail.com>
2018-07-08 21:13:27 -0700

    Update CircleGraphics.py

--- CircleGraphics.py
+++ CircleGraphics.py
@@ -7,3 +7,11 @@
 	    t.circle(50)
 	    t.end_fill()
 	mycircle(0, 0.5, 0)
+	
+#Circle Graphics
+	import turtle
+	t = turtle.Pen()
+	t.color(1,1,0) 
+	t.begin_fill() 
+	t.circle(50) 
+	t.end_fill()
