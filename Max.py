97957b0889f209b2e07257b82b009c96bfdbed81
ericaandrews <eureka.web.dev@gmail.com>
2018-07-08 18:29:48 -0700

    Create Max.py

---
+++ Max.py
@@ -0,0 +1,22 @@
+#Max    
+	list=[1,3,6,8,4,9,23,45,67,89]
+	print(max(list))
+	
+	my_letters= ('m,y,l,e,t,t,e,r,s')
+	print(max(my_letters))
+	
+	print(max(10, 300, 450, 50, 90))
+	
+	player_guesses= [12,6,45,8,23]
+	if max(player_guesses) > 25:
+	    print("You win!")
+	else:
+	    print("You lose!")
+
+	guess_this_number = 25
+	player_guesses=[2,3,5,4,7,6,8]
+	if max(player_guesses) > guess_this_number:
+	    print("You win!")
+	else:
+	    print("You lose!")	
+	
