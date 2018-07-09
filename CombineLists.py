1aaca26e8c2afea99b8a0888f182494b4c18c361
ericaandrews <eureka.web.dev@gmail.com>
2018-07-08 18:23:16 -0700

    Create CombineLists.py

---
+++ CombineLists.py
@@ -0,0 +1,8 @@
+#Combine lists
+	activities = ['reading', 'walking', 'cooking', 'driving', 'hanging out']
+	print(activities)
+	foods = ['pastas', 'sushi', 'tacos', 'sandwitches', 'salads']
+	print(foods)
+	favorites = (activities + foods)
+	print (favorites)
+			
