cca821c83eb9790f2029c3cd0eae8cfbaa49956d
ericaandrews <eureka.web.dev@gmail.com>
2018-07-08 18:26:34 -0700

    Create Keyword.py

---
+++ Keyword.py
@@ -0,0 +1,8 @@
+#Keyword
+import keyword
+	print(keyword.kwlist)
+	
+	#Check if word is keyword
+	print(keyword.iskeyword('if'))
+	print(keyword.iskeyword('else'))
+	print(keyword.iskeyword('keyword'))
