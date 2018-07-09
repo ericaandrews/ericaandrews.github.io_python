b5c6ddb1ddcd122fb22743fdb0d3cc15263f63c7
ericaandrews <eureka.web.dev@gmail.com>
2018-07-08 18:25:28 -0700

    Create AbsoluteFunction.py

---
+++ AbsoluteFunction.py
@@ -0,0 +1,21 @@
+#Absolute function
+	print(abs(10))
+	print(abs(-10))
+
+	a = abs(10) + abs(-10)
+	print(a)
+
+	b = abs(-10) + -10
+	print(b)
+
+	steps = -3
+	if abs(steps) > 0:
+	    print('Character is moving')
+	else: 
+	    print('Character is not moving')
+    
+	steps = -3
+	if (steps) > 0:
+	    print('Character is moving')    
+	else: 
+	    print('Character is not moving')
