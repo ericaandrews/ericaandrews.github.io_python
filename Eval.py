73a3aae6e47149deede9195425387d444e8dedb4
ericarandrews <eureka.web.dev@gmail.com>
2019-08-21 18:40:42 -0700

    Update Eval.py

--- Eval.py
+++ Eval.py
@@ -1,5 +1,23 @@
 #Eval    
-	calculations= input(" ")
-	#Enter calculation; Press enter/return;
-	eval(calculations)
+	eval("5 == 5")
+	eval("4 < 10")
+	eval("8 + 4 - 2 * 3")
+	eval("'py ' * 5")
+	eval("10 ** 2")
+	eval("'hello' + 'py'")
+	eval("abs(-11)")
+	eval('"hello".upper()')
 	
+	#Reference variable
+	x = 2
+	eval("x+4")
+	
+	#Get current working directory
+	import os
+	eval('os.getcwd()')
+	
+
+
+
+
+
