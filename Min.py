10285b89bc3622da9f6f6c9d11700a1de6c5c784
ericarandrews <eureka.web.dev@gmail.com>
2020-01-05 14:11:54 -0800

    Update Min.py

--- Min.py
+++ Min.py
@@ -1,5 +1,21 @@
 #Min
-	list=[3,6,8,2,34,64,76,12,34]
+	list=[1,3,6,8,4,9,23,45,67,89]
 	print(min(list))
 	
+	my_letters= ('m,y,l,e,t,t,e,r,s')
+	print(min(my_letters))
 	
+	print(min(10, 300, 450, 50, 90))
+	
+	player_guesses= [12,6,45,8,23]
+	if min(player_guesses) > 25:
+	    print("You win!")
+	else:
+	    print("You lose!")
+
+	guess_this_number = 25
+	player_guesses=[2,3,5,4,7,6,8]
+	if min(player_guesses) > guess_this_number:
+	    print("You win!")
+	else:
+	    print("You lose!")	
