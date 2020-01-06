e84a9dda996c4452b7460526d8dd3d3d6f8cf5a9
ericarandrews <eureka.web.dev@gmail.com>
2020-01-05 20:46:54 -0800

    Update SysStdinReadline.py

--- SysStdinReadline.py
+++ SysStdinReadline.py
@@ -1,9 +1,12 @@
 #sys.stdin.readline()
 	def your_age():
-	    print('How old are you?')
+	    print('Enter how old are you:')
 	    age = int(sys.stdin.readline())
+	    if age < 10: 
+		print('Huh? That is too young!')
 	    if age >= 10 and age <= 13:
-		print('What is 13 + 49 + 84 + 155 + 97? A headache!')
-	    else:
-		print('Huh?')
+		print('Joke: What is 13 + 49 + 84 + 155 + 97?')
+		print('Answer: A headache!')
+	    if age > 13:
+		print('Huh? That is too old!')
 	your_age()
